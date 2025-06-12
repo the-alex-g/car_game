75b223a9f83ab54a4d489634c15426b773906733
Alex G <alex.gestwicki@gmail.com>
2025-06-12 14:29:44 -0400

    Support two-dimensional speed/acceleration

--- project/car/car.gd
+++ project/car/car.gd
@@ -10,47 +10,47 @@ extends CharacterBody2D
 @export var braking := 300.0
 @export var max_speed_reverse := 150.0
 
-var _acceleration := 0.0
+var _acceleration := Vector2.ZERO
 var _steer_direction := 0.0
-var _speed := 0.0 :
-	set(value):
-		_speed = max(value, -max_speed_reverse)
 
 
 func _physics_process(delta: float) -> void:
-	_acceleration = 0.0
+	_acceleration = Vector2.ZERO
 	_get_input()
 	_apply_friction()
-	_speed += _acceleration * delta
+	velocity += _acceleration * delta
 	_calculate_steering(delta)
-	var collision := move_and_collide(transform.x * _speed * delta)
+	var collision := move_and_collide(velocity * delta)
 	if collision:
-		_speed = -_speed / 2.0
-		_acceleration = 0.0
+		velocity = -velocity / 2.0
 
 
 func _get_input() -> void:
 	var turn := Input.get_axis("left", "right")
 	_steer_direction = turn * steering_angle
 	if Input.is_action_pressed("forward"):
-		_acceleration = engine_power
+		_acceleration = transform.x * engine_power
 	if Input.is_action_pressed("backward"):
-		_acceleration = -braking
+		_acceleration = transform.x * -braking
 
 
 func _apply_friction() -> void:
-	if absf(_speed) < 5.0:
-		_speed = 0.0
-	var friction_force := absf(_speed) * friction
-	var drag_force := pow(absf(_speed), 2.0) * drag
-	_acceleration -= (drag_force + friction_force) * signf(_speed)
+	if velocity.length_squared() < 1.0:
+		velocity = Vector2.ZERO
+	var friction_force := velocity * friction
+	var drag_force := velocity * velocity.length() * drag
+	_acceleration -= (drag_force + friction_force)
 
 
 func _calculate_steering(delta: float) -> void:
 	var rear_wheel := position - transform.x * wheel_base / 2
 	var front_wheel := position + transform.x * wheel_base / 2
-	rear_wheel += transform.x * _speed * delta
-	front_wheel += transform.x.rotated(_steer_direction) * delta * _speed
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(_steer_direction) * delta
 	var heading := (front_wheel - rear_wheel).normalized()
-	velocity = heading * _speed
+	var dot_product := heading.dot(velocity.normalized())
+	if dot_product > 0:
+		velocity = heading * velocity.length()
+	elif dot_product < 0:
+		velocity = -heading * minf(velocity.length(), max_speed_reverse)
 	rotation = heading.angle()
