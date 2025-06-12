85f2a60b43532c5be3bdd9e114585a5571420eb8
Alex G <alex.gestwicki@gmail.com>
2025-06-12 14:59:28 -0400

    Add bouncy car collisions

--- project/car/car.gd
+++ project/car/car.gd
@@ -1,14 +1,18 @@
+class_name Car
 extends CharacterBody2D
 
-# car movement based on this video: https://www.youtube.com/watch?v=mJ1ZfGDTMCY
+# car movement from this video: https://www.youtube.com/watch?v=mJ1ZfGDTMCY
 
 @export var wheel_base := 8.0
 @export var steering_angle := PI / 30
-@export var engine_power := 400.0
+@export var engine_power := 300.0
 @export var friction := 0.9
 @export var drag := 0.001
-@export var braking := 300.0
-@export var max_speed_reverse := 150.0
+@export var braking := 100.0
+@export var max_speed_reverse := 100.0
+@export var sideways_push_resistance := 0.98
+@export var index := 0
+@export var disabled := false
 
 var _acceleration := Vector2.ZERO
 var _steer_direction := 0.0
@@ -22,16 +26,28 @@ func _physics_process(delta: float) -> void:
 	_calculate_steering(delta)
 	var collision := move_and_collide(velocity * delta)
 	if collision:
+		if collision.get_collider() is Car:
+			collision.get_collider().velocity += velocity / 2.0
 		velocity = -velocity / 2.0
 
 
 func _get_input() -> void:
-	var turn := Input.get_axis("left", "right")
+	if disabled:
+		return
+	
+	var turn := Input.get_joy_axis(index, JOY_AXIS_LEFT_X)
+	if index == 0 and turn == 0:
+		turn = Input.get_axis("left", "right")
 	_steer_direction = turn * steering_angle
-	if Input.is_action_pressed("forward"):
+	if Input.is_action_pressed("forward") and index == 0:
 		_acceleration = transform.x * engine_power
-	if Input.is_action_pressed("backward"):
-		_acceleration = transform.x * -braking
+	if Input.is_action_pressed("backward") and index == 0:
+		_acceleration = -transform.x * braking
+	
+	if _acceleration == Vector2.ZERO:
+		_acceleration = transform.x * remap(
+			Input.get_joy_axis(index, JOY_AXIS_RIGHT_Y), 1.0, -1.0, -braking, engine_power
+		)
 
 
 func _apply_friction() -> void:
@@ -43,14 +59,17 @@ func _apply_friction() -> void:
 
 
 func _calculate_steering(delta: float) -> void:
-	var rear_wheel := position - transform.x * wheel_base / 2
-	var front_wheel := position + transform.x * wheel_base / 2
+	var rear_wheel := global_position - transform.x * wheel_base / 2
+	var front_wheel := global_position + transform.x * wheel_base / 2
 	rear_wheel += velocity * delta
 	front_wheel += velocity.rotated(_steer_direction) * delta
 	var heading := (front_wheel - rear_wheel).normalized()
 	var dot_product := heading.dot(velocity.normalized())
+	var forward_speed := velocity.project(transform.x).length()
+	var sideways_velocity := velocity.project(transform.y)
 	if dot_product > 0:
-		velocity = heading * velocity.length()
+		velocity = heading * forward_speed
 	elif dot_product < 0:
-		velocity = -heading * minf(velocity.length(), max_speed_reverse)
+		velocity = -heading * minf(forward_speed, max_speed_reverse)
+	velocity += sideways_velocity * sideways_push_resistance
 	rotation = heading.angle()
