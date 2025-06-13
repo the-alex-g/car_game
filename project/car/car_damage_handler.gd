a175ead1a113823b286ba6dd393f8bcac81376b9
Alex G <alex.gestwicki@gmail.com>
2025-06-13 15:18:42 -0400

    Switch from arrays to images

--- project/car/car_damage_handler.gd
+++ project/car/car_damage_handler.gd
@@ -1,33 +1,32 @@
 extends Node
 
-var base_damage_array : Array[PackedByteArray] = []
-var car_size := Vector2.ZERO
-var car_damage_arrays : Dictionary = {}
+var base_damage_image : Image
+var car_size := Vector2i.ZERO
+var car_damage_images : Dictionary = {}
 
 
 func _init() -> void:
-	var car_template := preload("res://car/images/car_red_2.png").get_image()
-	for x in car_template.get_size().x:
-		car_size.y += 1
-		var row : PackedByteArray = []
-		for y in car_template.get_size().y:
-			if car_template.get_pixel(x, y).a > 0.25:
-				row.append(0b0000_0000)
+	var template := preload("res://car/images/car_red_2.png").get_image()
+	car_size = Vector2i(template.get_height(), template.get_width())
+	base_damage_image = Image.create_empty(car_size.x, car_size.y, false, Image.FORMAT_BPTC_RGBA)
+	base_damage_image.decompress()
+	for x in car_size.x:
+		for y in car_size.y:
+			if template.get_pixel(y, x).a > 0.25:
+				base_damage_image.set_pixel(x, y, Color(0.0, 0.0, 1.0, 1.0))
 			else:
-				row.append(0b1000_0000)
-		base_damage_array.append(row)
-	car_size.x = base_damage_array[0].size()
+				base_damage_image.set_pixel(x, y, Color(0.0, 0.0, 0.0, 0.0))
 
 
 func log_car(index: int) -> void:
-	car_damage_arrays[index] = base_damage_array.duplicate(true)
+	car_damage_images[index] = base_damage_image.duplicate()
 
 
-func get_value(index: int, position: Vector2) -> int:
+func get_value(index: int, position: Vector2) -> Color:
 	if point_in_bounds(position):
-		return car_damage_arrays[index][position.y][position.x]
+		return car_damage_images[index].get_pixelv(position)
 	else:
-		return 0
+		return Color(0.0, 0.0, 0.0, 0.0)
 
 
 func point_in_bounds(position: Vector2) -> bool:
@@ -36,27 +35,19 @@ func point_in_bounds(position: Vector2) -> bool:
 
 
 func point_exists(index: int, position: Vector2) -> bool:
-	return point_in_bounds(position) and not get_value(index, position) && 0b1000_0000
+	var value := get_value(index, position)
+	return point_in_bounds(position) and value.a > 0.0 and value.r < value.b
 
 
 func damage_car(index: int, amount: float, offset: Vector2, radius := 10) -> void:
-	var position := offset + car_size / 2
+	var position := offset + Vector2(car_size) / 2
 	for x in range(position.x - radius, position.x + radius):
 		for y in range(position.y - radius, position.y + radius):
 			if point_exists(index, Vector2(x, y)):
-				car_damage_arrays[index][y][x] += roundi(amount * 0b0111_1111)
+				var current_value := get_value(index, Vector2(x, y))
+				current_value.r += amount
+				car_damage_images[index].set_pixel(x, y, current_value)
 
 
 func generate_car_texture(index: int) -> ImageTexture:
-	var image := Image.create_empty(car_size.x, car_size.y, false, Image.FORMAT_BPTC_RGBA)
-	image.decompress()
-	var y := 0
-	for row : PackedByteArray in car_damage_arrays[index]:
-		var x := 0
-		for point in row:
-			if !(point && 0b1000_0000):
-				var value := point / float(0b0111_1111)
-				image.set_pixel(x, y, Color(value, value, value, 1.0))
-			x += 1
-		y += 1
-	return ImageTexture.create_from_image(image)
+	return ImageTexture.create_from_image(car_damage_images[index])
